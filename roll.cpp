d76be6c7755fe1cbae471d43510530b5b46e8822
panmau <16801528+panmau@users.noreply.github.com>
2021-10-15 16:51:05 +0200

    vectors, yay

--- roll.cpp
+++ roll.cpp
@@ -1,13 +1,26 @@
 #include "roll.h"
+#include <vector>
+#include <algorithm>
+
+void roll (std::vector<int>& rollResults, int diceCount, Dice d) {
+    for (int i = 0; i < diceCount; ++i) {
+        rollResults.push_back(::roll(d));
+    }
+}
 
 Roll::Roll(int diceCount, Dice d, int bonus) {
     _diceCount = diceCount;
     _dice = d;
     _bonus = bonus;
     int result = bonus;
-    for (int i = 0; i < diceCount; ++i) {
-        result += ::roll(_dice);
-    }
+
+    roll(_rollResults, _diceCount, _dice);
+
+    std::for_each(_rollResults.begin(), _rollResults.end(),
+                  [&](int rollResult) { result += rollResult; });
+
+
+
     _result = result;
 }
 
@@ -25,4 +38,8 @@ int Roll::getBonus() const {
 
 int Roll::getResult() const {
     return _result;
+}
+
+const std::vector<int>& Roll::getRollResults() const {
+    return _rollResults;
 }
--- roll.h
+++ roll.h
@@ -3,6 +3,7 @@
 
 #include <ostream>
 #include "dice.h"
+#include <vector>
 
 class Roll {
 public:
@@ -11,11 +12,13 @@ public:
     Dice getDice() const;
     int getBonus() const;
     int getResult() const;
+    const std::vector<int>& getRollResults() const;
 private:
     int _diceCount;
     Dice _dice;
     int _bonus;
     int _result;
+    std::vector<int> _rollResults;
 };
 
 inline std::ostream& operator<<(std::ostream& os, const Roll& roll)
@@ -26,7 +29,21 @@ inline std::ostream& operator<<(std::ostream& os, const Roll& roll)
     << "+"
     << roll.getBonus()
     << " = "
-    << roll.getResult();
+    << roll.getResult()
+    << " [";
+
+    auto rollResults = roll.getRollResults();
+    for(auto it = rollResults.begin(); it != rollResults.end(); ++it) {
+        int roll = *it;
+        if (rollResults.end() - 1 == it) {
+            os << roll;
+        } else {
+            os << roll << ", ";
+        }
+    }
+
+    os << "]";
+
     return os;
 }
 #endif //CROLL_ROLL_H
